80dbc315babacaea5e6dd585990825a909113e7a
Thanh Tran <thaithanh2405@gmail.com>
2023-05-04 21:38:16 +0700

    Add attibute to access environment test modules and folders

--- Source/CANoe_Lib/TestEnvironment_Folder.py
+++ Source/CANoe_Lib/TestEnvironment_Folder.py
@@ -1,13 +1,13 @@
 from .TestModules import TestModules
+from .TestEnvironment_Folders import TestEnvironemntFolders
 
 class TestEnvironmentFolder:
     def __init__(self, test_environemnt_folder_object):
         self.test_environemnt_folder = test_environemnt_folder_object
         self.name = self.test_environemnt_folder.Name
         self.enable = self.test_environemnt_folder.Enabled
+        self.test_modules = TestModules(self.test_environemnt_folder.TestModules)
+        self.folders = TestEnvironemntFolders(self.test_environemnt_folder.Folders)
 
     def execute_all(self):
         self.test_environemnt_folder.ExecuteAll()
-
-    def get_list_test_modules(self):
-        self.test_environemnt_folder.TestModules
